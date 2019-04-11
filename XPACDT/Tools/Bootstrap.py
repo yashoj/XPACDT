d3a190bbcbd32d7d68ac1ba11047447a87425326
Ralph Welsch <ralph.welsch@cfel.de>
2019-04-11 14:00:31 +0200

    First try on bootstrap. Not tested and documented fully.

---
+++ XPACDT/Tools/Bootstrap.py
@@ -0,0 +1,64 @@
+#  **************************************************************************
+#
+#  XPACDT, eXtended PolyAtomic Chemical Dynamics Toolkit
+#  XPACDT is a program that can treat polyatomic chemical dynamics problems
+#  by solving Newtons equations of motion directly or in an
+#  extended ring-polymer phase space. Non-Born-Oppenheimer effects are
+#  included employ different approaches, including fewest switches surface
+#  hopping.
+#
+#  Copyright (C) 2019
+#  Ralph Welsch, DESY, <ralph.welsch@desy.de>
+#
+#  This file is part of XPACDT.
+#
+#  XPACDT is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  **************************************************************************
+
+""" Module to perform bootstrapping analysis."""
+
+import numpy as np
+
+
+def bootstrap(data, function, n_bootstrap=1000):
+    """
+
+    Parameters
+    ----------
+    data : ndarray of floats
+        The data.
+    function : function
+        The function for which the bootstrapping should be calculate, e.g..
+        np.mean, np.percentile, histogram, ...
+    n_bootstrap : int, optional
+        Number of bootstrapping resamples to take. Default: 1000
+
+    Returns:
+        ndarrays with the mean value of the function results and corresponding error.
+
+### lambda y: np.histogram(y, bins=30, range=(-3.0,3.0), density=True)[0]
+    """
+
+    nsamples = len(data)
+    if n_bootstrap == 1:
+        values = [function(data)]
+    else:
+        values = [function(data[np.random.randint(nsamples, size=nsamples)])
+                  for i in range(n_bootstrap)]
+
+    m = np.mean(np.array(values).reshape(n_bootstrap, -1), axis=0)
+    s = np.std(np.array(values).reshape(n_bootstrap, -1), axis=0)
+
+    return m, s

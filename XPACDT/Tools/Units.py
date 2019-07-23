Some fixes for the units part.

XPACDT/Tools/Units.py
@@ -31,6 +31,7 @@
 
 
 from scipy.constants import physical_constants, femto, pico, atto, centi, pi
+# TODO: Implement our own periodic table
 # from mendeleev import element
 from pyne.data import atomic_mass
 
@@ -65,7 +66,11 @@ def atom_mass(symbol):
     """ Return the mass of an atom in atomic units.
     """
 
+    # TODO: Better element parsing; isotope parsing!
     conversion = physical_constants['atomic mass constant'][0] / physical_constants['atomic unit of mass'][0]
-    return atomic_mass(symbol) * conversion
-#    return atomic_mass(symbol) * conversion
-#    return element(symbol).atomic_weight * conversion
+    try:
+        return atomic_mass(symbol) * conversion
+        #    return atomic_mass(symbol) * conversion
+        # return element(symbol).atomic_weight * conversion
+    except:
+        raise RuntimeError("Error obtaining element weight: ", symbol)

XPACDT/UnitTests/test_Units.py
@@ -42,7 +42,7 @@ class UnitsTest(unittest.TestCase):
 #        self.input = infile.Inputfile("input.in")
 
     def test_atom_mass(self):
-        # from 
+        # from https://en.wikipedia.org/wiki/Electron_rest_mass
         amu_to_au = 1.0 / 5.48579909065e-4
         # from http://www.ciaaw.org/atomic-weights.htm
         mass_h_ref = 1.0078250322 * amu_to_au
@@ -51,7 +51,6 @@ class UnitsTest(unittest.TestCase):
         mass_d_ref = 2.0141017781 * amu_to_au
         mass_o18_ref = 17.999159613 * amu_to_au
 
-# TODO: change places etc. test; also check NM test
         np.testing.assert_allclose(units.atom_mass('H'), mass_h_ref, rtol=1e-3)
         np.testing.assert_allclose(units.atom_mass('F'), mass_f_ref, rtol=1e-3)
         np.testing.assert_allclose(units.atom_mass('O'), mass_o_ref, rtol=1e-3)
Bugfix in units, added flexibility for derivative stepsizes, tests for 3 atom cases, but not for multiple beads

XPACDT/Interfaces/CW.py
@@ -47,6 +47,8 @@ class CW(itemplate.PotentialInterface):
         self.__data_path = os.path.dirname(pot.__file__) + "/"
         pot.pes_init()
         itemplate.PotentialInterface.__init__(self, "CW")
+        # For proper Hessian derivatives!
+        self._DERIVATIVE_STEPSIZE = 7e-3
 
     def _calculate_all(self, R, P=None, S=None):
         """
@@ -70,20 +72,22 @@ class CW(itemplate.PotentialInterface):
         assert (R.ndim == 2), "Position array not two-dimensional!"
         assert (R.dtype == 'float64'), "Position array not real!"
 
-        self._energy = np.zeros(R.shape[1])
-        self._gradient = np.zeros(R.shape)
+        self._energy = np.zeros((1, R.shape[1]))
+        self._gradient = np.zeros_like(R[np.newaxis, :])
 
         # centroid part if more than 1 bead
         if R.shape[1] > 1:
             centroid = np.mean(R, axis=1)
             self._energy_centroid, self._gradient_centroid = pot.pot(centroid)
+            self._energy_centroid = self._energy_centroid[np.newaxis, :]
+            self._gradient_centroid = self._gradient_centroid[np.newaxis, :]
 
         for i, r in enumerate(R.T):
-            self._energy[i], self._gradient[:, i] = pot.pot(r, self.__data_path)
+            self._energy[0, i], self._gradient[0, :, i] = pot.pot(r, self.__data_path)
 
         if R.shape[1] == 1:
-            self._energy_centroid = self._energy
-            self._gradient_centroid = self._gradient
+            self._energy_centroid = self._energy[:, 0]
+            self._gradient_centroid = self._gradient[:, :, 0]
 
         return
 

XPACDT/Interfaces/InterfaceTemplate.py
@@ -67,6 +67,7 @@ class PotentialInterface:
         self._gradient_gradient = None
 
         self.__SAVE_THRESHOLD = 1e-8
+        self._DERIVATIVE_STEPSIZE = 1e-4
 
     @property
     def name(self):
@@ -82,7 +83,7 @@ class PotentialInterface:
     @property
     def DERIVATIVE_STEPSIZE(self):
         """float : Step size for numerical derivatives in au."""
-        return 1e-4
+        return self._DERIVATIVE_STEPSIZE
 
     def _calculate_all(self, R, P, S=None):
         """Calculate the energy, gradient and possibly couplings at the current
@@ -505,6 +506,8 @@ class PotentialInterface:
             Hessian of the potential at the given geometry.
         """
 
+#        old_thresh = self.__SAVE_THRESHOLD
+#        self.__SAVE_THRESHOLD = 1e-15
         n = len(R)
         H = np.zeros((n, n))
         R_step = R.copy()
@@ -521,4 +524,5 @@ class PotentialInterface:
 
             R_step[i] += self.DERIVATIVE_STEPSIZE
 
+#        self.__SAVE_THRESHOLD = old_thresh
         return H
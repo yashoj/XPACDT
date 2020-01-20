20da33f07fec6b800feb9a66aa2fe8f59df3e1e3
Ralph Welsch <ralph.welsch@cfel.de>
2020-01-20 11:04:12 +0100

    Combined 3 atom pes to triatomic.

---
+++ XPACDT/Interfaces/Triatomic.py
@@ -0,0 +1,255 @@
+#  **************************************************************************
+#
+#  XPACDT, eXtended PolyAtomic Chemical Dynamics Toolkit
+#  XPACDT is a program that can treat polyatomic chemical dynamics problems
+#  by solving Newtons equations of motion directly or in an
+#  extended ring-polymer phase space. Non-Born-Oppenheimer effects are
+#  included employ different approaches, including fewest switches surface
+#  hopping.
+#
+#  Copyright (C) 2019
+#  Ralph Welsch, DESY, <ralph.welsch@desy.de>
+#
+#  This file is part of XPACDT.
+#
+#  XPACDT is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  **************************************************************************
+
+""" This module connects to any triatomic fitted PES.
+
+Implemented are:
+
+H3 - BKMP2:
+A. I. Boothroyd, W. J. Keogh, P. G. Martin, and M. R. Peterson,
+J. Chem. Phys. 104, 7139 (1996).
+
+ClH2 - CW:
+G. Capecchi and H. J. Werner, Phys. Chem. Chem. Phys. 6, 4975 (2004).
+
+FH2 - LWAL:
+G. Li, H.-J. Werner, F. Lique, and M. H. Alexander, J. Chem. Phys. 127, 174302 (2007).
+"""
+
+import importlib
+import numpy as np
+import os
+import sys
+
+import XPACDT
+import XPACDT.Interfaces.InterfaceTemplate as itemplate
+import XPACDT.Tools.Geometry as geom
+import XPACDT.Tools.Units as units
+
+# add some global variable that lists all implemented pes and masses
+available_pes = {'BKMP2': { 'masses': [units.atom_mass('H'), units.atom_mass('H'), units.atom_mass('H')]},
+                 'LWAL': { 'masses': [units.atom_mass('F'), units.atom_mass('H'), units.atom_mass('H')]},
+                 'CW': { 'masses': [units.atom_mass('Cl'), units.atom_mass('H'), units.atom_mass('H')]}
+                 }
+
+class Triatomic(itemplate.PotentialInterface):
+    """
+    Triatomic PES.
+
+    Parameters
+    ----------
+    max_n_beads : int, optional
+        Maximum number of beads from the (n_dof) list of n_beads. Default: 1.
+
+    Other Parameters
+    ----------------
+    name : string
+        The name of the PES requested.
+    """
+    def __init__(self, max_n_beads=1, **kwargs):
+        pes_name = kwargs.get('name')
+
+        if pes_name not in available_pes:
+            raise RuntimeError("\nXPACDT: The requested triatomic pes is not implemented: " + pes_name)
+            # TODO: Maybe print the available pes
+ 
+        self.__pot = importlib.import_module("XPACDT.Interfaces."+ pes_name + "_module.pot")
+
+        self.__pot.pes_init()
+        itemplate.PotentialInterface.__init__(self, pes_name, 9, 1,
+                                              max_n_beads, 'adiabatic')
+
+        self.__data_path = os.path.dirname(self.__pot.__file__) + "/"        
+        self.__masses = available_pes.get(pes_name).get('masses')
+        
+        if pes_name == 'CW':
+            # For proper Hessian derivatives! Numerically tested for stability!
+            self._DERIVATIVE_STEPSIZE = 7e-3
+        
+    def _calculate_adiabatic_all(self, R, P=None, S=None):
+        """
+        Calculate the value of the potential and the gradient at positions R.
+
+        Parameters:
+        -----------
+        R : (n_dof, n_beads) ndarray of floats
+            The positions of all beads in the system. The first axis is the
+            degrees of freedom and the second axis the beads.
+            Please note that Cartesian coordinates of the atoms are used here.
+        P : (n_dof, n_beads) ndarray of floats, optional
+            The momenta of all beads in the system. The first axis is the
+            degrees of freedom and the second axis the beads. This is not
+            used in this potential and thus defaults to None.
+        S : int, optional
+            The current electronic state. This is not used in this potential
+            and thus defaults to None.
+        """
+
+        assert (isinstance(R, np.ndarray)), "R not a numpy array!"
+        assert (R.ndim == 2), "Position array not two-dimensional!"
+        assert (R.dtype == 'float64'), "Position array not real!"
+
+        self._adiabatic_energy = np.zeros((1, R.shape[1]))
+        self._adiabatic_gradient = np.zeros_like(R[np.newaxis, :])
+
+        self._adiabatic_energy_centroid = np.zeros(1)
+        self._adiabatic_gradient_centroid = np.zeros((1, R.shape[0]))
+
+        self._energy_centroid = np.zeros(1)
+        self._gradient_centroid = np.zeros((1, R.shape[0]))
+
+        # centroid part if more than 1 bead
+        if R.shape[1] > 1:
+            centroid = np.mean(R, axis=1)
+            self._adiabatic_energy_centroid[0], self._adiabatic_gradient_centroid[0] = self.__pot.pot(centroid, self.__data_path)
+
+        for i, r in enumerate(R.T):
+            self._adiabatic_energy[0, i], self._adiabatic_gradient[0, :, i] = self.__pot.pot(r, self.__data_path)
+
+        if R.shape[1] == 1:
+            self._adiabatic_energy_centroid = self._adiabatic_energy[:, 0]
+            self._adiabatic_gradient_centroid = self._adiabatic_gradient[:, :, 0]
+
+        return
+
+    def _from_cartesian_to_internal(self, R):
+        """Transform from full cartesian coordinates to internal Jacobi
+        coordinates. The Jacobi coordinates are defined as follows:
+            r = internal[0] = Distance between the second and third atom in au.
+            R = internal[1] = Distance between the first atom and the center of
+                              mass of the last two atoms in au.
+            phi = internal[2] = angle between the two vectors that define
+                                r and R.
+
+        Parameters
+        ----------
+        R: (9) ndarray of floats
+            Values of the cartesian coordinates in au.
+
+        Returns
+        -------
+        internal : (3) ndarray of floats
+            Values of the Jacobi coordinates associated with the input
+            cartesian coordinates in au.
+        """
+        internal = np.zeros(3)
+
+        # r (Always the last two atoms).
+        r_vec = R[3:6]-R[6:9]
+        internal[0] = np.linalg.norm(r_vec)
+        
+        # R 
+        R_vec = (1.0/(self.__masses[1]+self.__masses[2])) * (self.__masses[1]*R[3:6] + self.__masses[2]*R[6:9]) - R[0:3]
+        internal[1] = np.linalg.norm(R_vec)
+
+        # phi
+        internal[2] = geom.angle(r_vec, R_vec)
+        # Correct angle definition to range :math:`0 : 2\pi`
+        if R[1] < 0.0:
+            internal[2] = 2.0*np.pi-internal[2]
+
+        return internal
+
+    def _from_internal(self, internal):
+        return self._from_internal_to_cartesian(internal)
+
+    def _from_internal_to_cartesian(self, internal):
+        """Transform from Jacobi coordinates to full cartesian coordinates. The
+        Jacobi coordinates are defined as follows:
+            r = internal[0] = Distance between the second and third atom in au.
+            R = internal[1] = Distance between the first atom and the center of
+                              mass of the last two atoms in au.
+            phi = internal[2] = angle between the two vectors that define
+                                r and R.
+        The output cartesian coordinates are in the xy plane. The center of
+        mass of the last two atoms is fixed to the origin. The last two atoms are 
+        displaced along the x-axis in negative and positive direction, respectively. 
+        The first atom is then placed in the xy-plane according to 'R' and 'phi'.
+
+        Parameters
+        ----------
+        internal: (3) ndarray of floats
+            Values of the Jacobi coordinates in au.
+
+        Returns
+        -------
+        R : (9) ndarray of floats
+            Cartesian coordinates associated with the given internal
+            coordinates with the orientation defined above.
+        """
+
+        R = np.zeros(9)
+
+        # from r, fixed to x-axis
+        R[3] = -self.__masses[1]*internal[0] / (self.__masses[1] + self.__masses[2])
+        R[6] = self.__masses[2]*internal[0] / (self.__masses[1] + self.__masses[2])
+
+        # from R and phi, fixed to x-y-plane
+        R[0] = (internal[1]) * np.cos(internal[2])
+        R[1] = (internal[1]) * np.sin(internal[2])
+
+        return R
+
+
+#if __name__ == "__main__":
+#    pes = BKMP2()
+#    print(pes.name)
+#    x=np.zeros(9)
+#    for i in range(1000):
+#        phi = np.random.rand(1)*2.0*np.pi
+#        x[0] = -1.0
+#        x[1] = 0.0
+#        x[2] = 0.0 
+#        x[3] = 1.0
+#        x[4] = 0.0
+#        x[5] = 0.0
+#        x[6] = 3.0*np.cos(phi)
+#        x[7] = 3.0*np.sin(phi)
+#        x[8] = 0.0
+#        
+#        
+#        if phi > -11.0:
+#            inte = pes._from_cartesian_to_internal(x)
+##            print(phi, inte[2], 2*np.pi-inte[2], inte[2]+phi, inte[2]-phi)
+#            y = pes._from_internal_to_cartesian(inte)
+##            print(x, y)
+#   
+#            print((abs(x-y) < 1e-8).all())
+##            print()
+#    
+#    pes._calculate_all(x[:, None])
+#    print(pes._adiabatic_energy, pes._gradient)
+#    print(pes.adiabatic_energy(x[:, None]))
+#    internal = np.array([2.0, 5.0, 0.1])
+#    pes.plot_1D(internal, 1, 4.0, 7.0, 0.1, relax=True, internal=True)
+##    pes.plot_1D(internal, 0, 2.0, 10.0, 0.1, relax=True)
+#    
+#    pes.plot_2D(internal, 0, 1, (0.5, 2.0), (3.5, 7.0), (0.2, 0.2), relax=True, internal=True)
+##    pes.plot_2D(internal, 0, 1, (0.5, 2.0), (3.5, 7.0), (0.2, 0.2), relax=True, internal=True)
+##    pes.plot_2D(internal, 2, 0.0, 2*np.pi, 0.1, relax=True, internal=True)
---
+++ XPACDT/UnitTests/test_Triatomic.py
@@ -0,0 +1,319 @@
+#!/usr/bin/env python3
+
+#  **************************************************************************
+#
+#  XPACDT, eXtended PolyAtomic Chemical Dynamics Toolkit
+#  XPACDT is a program that can treat polyatomic chemical dynamics problems
+#  by solving Newtons equations of motion directly or in an
+#  extended ring-polymer phase space. Non-Born-Oppenheimer effects are
+#  included employ different approaches, including fewest switches surface
+#  hopping.
+#
+#  Copyright (C) 2019
+#  Ralph Welsch, DESY, <ralph.welsch@desy.de>
+#
+#  This file is part of XPACDT.
+#
+#  XPACDT is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  **************************************************************************
+
+import numpy as np
+import unittest
+
+import XPACDT.Interfaces.Triatomic as triatomic
+import XPACDT.Tools.NormalModes as nm
+import XPACDT.Tools.Units as units
+
+
+class TriatomicTest(unittest.TestCase):
+
+    def setUp(self):
+        self.pes_bkmp2 = triatomic.Triatomic(**{'name': 'BKMP2'})
+        self.pes_cw = triatomic.Triatomic(**{'name': 'CW'})
+        self.pes_lwal = triatomic.Triatomic(**{'name': 'LWAL'})
+
+    def test_creation(self):
+        self.assertEqual(self.pes_bkmp2.name, 'BKMP2')
+        self.assertEqual(self.pes_cw.name, 'CW')
+        self.assertEqual(self.pes_lwal.name, 'LWAL')
+
+    def test_calculate_adiabatic_all(self):
+        # BKMP2
+        # Full Asymptote
+        energy_ref = np.zeros((1, 1))
+        gradient_ref = np.zeros((1, 9, 1))
+        self.pes_bkmp2._calculate_adiabatic_all(self.pes_bkmp2._from_internal([400.0, 800.0, 0.0]).reshape(-1, 1), None)
+        np.testing.assert_allclose(self.pes_bkmp2._adiabatic_energy, energy_ref, atol=1e-5)
+        np.testing.assert_allclose(self.pes_bkmp2._adiabatic_gradient, gradient_ref, atol=1e-10)
+
+        # H2 minimum 
+        energy_ref = np.zeros((1, 1)) + -0.17449577
+        gradient_ref = np.zeros((1, 9, 1))
+        self.pes_bkmp2._calculate_adiabatic_all(self.pes_bkmp2._from_internal([1.4014718, 80.0, 0.0]).reshape(-1, 1), None)
+        np.testing.assert_allclose(self.pes_bkmp2._adiabatic_energy, energy_ref)
+        np.testing.assert_allclose(self.pes_bkmp2._adiabatic_gradient, gradient_ref, atol=1e-6)
+
+        # TST
+        energy_ref = np.zeros((1, 1)) + -0.17449577 + 0.01532
+        gradient_ref = np.zeros((1, 9, 1))
+        self.pes_bkmp2._calculate_adiabatic_all(self.pes_bkmp2._from_internal([1.757, 2.6355, 0.0]).reshape(-1, 1), None)
+        np.testing.assert_allclose(self.pes_bkmp2._adiabatic_energy, energy_ref, atol=1e-6)
+        np.testing.assert_allclose(self.pes_bkmp2._adiabatic_gradient, gradient_ref, atol=1e-10)
+
+        # RPMD
+        x0 = self.pes_bkmp2._from_internal([1.757, 2.6355, 0.0])
+        x1 = self.pes_bkmp2._from_internal([400.0, 800.0, 0.0])
+        x2 = self.pes_bkmp2._from_internal([1.4014718, 80.0, 0.0])
+        x3 = self.pes_bkmp2._from_internal([1.757, 2.6355, 0.0])
+        x = np.column_stack((x0, x1, x2, x3))
+        self.pes_bkmp2._calculate_adiabatic_all(x, None)
+        energy_ref = np.array([[-0.15917577, 0.0, -0.17449577, -0.15917577]])
+        gradient_ref = np.zeros((1, 9, 4))
+        np.testing.assert_allclose(self.pes_bkmp2._adiabatic_energy, energy_ref, atol=1e-5)
+        np.testing.assert_allclose(self.pes_bkmp2._adiabatic_gradient, gradient_ref, atol=1e-6)
+
+        # LWAL
+        # H2 minimum
+        energy_ref = np.zeros((1, 1)) 
+        gradient_ref = np.zeros((1, 9, 1))
+        self.pes_lwal._calculate_adiabatic_all(self.pes_lwal._from_internal([1.401168, 40.0, 0.0]).reshape(-1, 1), None)
+        np.testing.assert_allclose(self.pes_lwal._adiabatic_energy, energy_ref, atol=1e-5)
+        np.testing.assert_allclose(self.pes_lwal._adiabatic_gradient, gradient_ref, atol=1e-5)
+
+        # HF minimum
+        energy_ref = np.zeros((1, 1)) - 0.050454
+        gradient_ref = np.zeros((1, 9, 1))
+        self.pes_lwal._calculate_adiabatic_all(self.pes_lwal._from_internal([80.0, 40.0+1.7335, 0.0]).reshape(-1, 1), None)
+        np.testing.assert_allclose(self.pes_lwal._adiabatic_energy, energy_ref, atol=1e-6)
+        np.testing.assert_allclose(self.pes_lwal._adiabatic_gradient, gradient_ref, atol=1e-4)
+
+        # co-linear TST
+        energy_ref = np.zeros((1, 1)) + 0.003428
+        gradient_ref = np.zeros((1, 9, 1))
+        self.pes_lwal._calculate_adiabatic_all(self.pes_lwal._from_internal([1.443, 2.936+0.5*1.443, 0.0]).reshape(-1, 1), None)
+        np.testing.assert_allclose(self.pes_lwal._adiabatic_energy, energy_ref, atol=1e-5)
+        save_adiabatic_gradient = self.pes_lwal._adiabatic_gradient_centroid
+
+        # TST
+        energy_ref = np.zeros((1, 1)) + 0.002616
+        gradient_ref = np.zeros((1, 9, 1))
+        R = np.sqrt( (1.457/2.0)**2 + 2.932**2 - 1.457*2.932*np.cos(np.pi*113.0/180.0))
+        gamma = np.arccos((2.932**2 - R**2 - (1.457/2.0)**2) / (-R * 1.457))
+        self.pes_lwal._calculate_adiabatic_all(self.pes_lwal._from_internal([1.457, R, gamma]).reshape(-1, 1), None)
+        np.testing.assert_allclose(self.pes_lwal._adiabatic_energy, energy_ref, atol=1e-6)
+        np.testing.assert_allclose(self.pes_lwal._adiabatic_gradient, gradient_ref, atol=1e-4)
+
+        # RPMD
+        x0 = self.pes_lwal._from_internal([1.401168, 40.0, 0.0])
+        x1 = self.pes_lwal._from_internal([80.0, 40.0+1.7335, 0.0])
+        x2 = self.pes_lwal._from_internal([1.443, 2.936+0.5*1.443, 0.0])
+        x3 = self.pes_lwal._from_internal([1.457, R, gamma])
+        x = np.column_stack((x0, x1, x2, x3))
+        self.pes_lwal._calculate_adiabatic_all(x, None)
+        energy_ref = np.array([[0.0, -0.050454, 0.003428, 0.002616]])
+        gradient_ref = np.zeros((1, 9, 4))
+        gradient_ref[0, :, 2] = save_adiabatic_gradient[0]
+        np.testing.assert_allclose(self.pes_lwal._adiabatic_energy, energy_ref, atol=1e-5)
+        np.testing.assert_allclose(self.pes_lwal._adiabatic_gradient, gradient_ref, atol=1e-4)
+
+        # CW
+        # H2 minimum 
+        energy_ref = np.zeros((1, 1)) - 0.00139  # lowered by SO
+        gradient_ref = np.zeros((1, 9, 1))
+        self.pes_cw._calculate_adiabatic_all(self.pes_cw._from_internal([1.4005706, 40.0, 0.0]).reshape(-1, 1), None)
+        np.testing.assert_allclose(self.pes_cw._adiabatic_energy, energy_ref, atol=1e-6)
+        np.testing.assert_allclose(self.pes_cw._adiabatic_gradient, gradient_ref, atol=1e-4)
+
+        # HCl minimum 
+        energy_ref = np.zeros((1, 1)) + 0.004114
+        gradient_ref = np.zeros((1, 9, 1))
+        self.pes_cw._calculate_adiabatic_all(self.pes_cw._from_internal([120.0, 60.0+2.41003, 0.0]).reshape(-1, 1), None)
+        np.testing.assert_allclose(self.pes_cw._adiabatic_energy, energy_ref, atol=1e-6)
+        np.testing.assert_allclose(self.pes_cw._adiabatic_gradient, gradient_ref, atol=1e-4)
+
+        # TST
+        energy_ref = np.zeros((1, 1)) + 0.012107
+        gradient_ref = np.zeros((1, 9, 1))
+        self.pes_cw._calculate_adiabatic_all(self.pes_cw._from_internal([1.854, 3.631, 0.0]).reshape(-1, 1), None)
+        np.testing.assert_allclose(self.pes_cw._adiabatic_energy, energy_ref, atol=1e-5)
+        np.testing.assert_allclose(self.pes_cw._adiabatic_gradient, gradient_ref, atol=1e-4)
+
+        # RPMD
+        x0 = self.pes_cw._from_internal([1.854, 3.631, 0.0])
+        x1 = self.pes_cw._from_internal([1.4005706, 40.0, 0.0])
+        x2 = self.pes_cw._from_internal([120.0, 60.0+2.41003, 0.0])
+        x3 = self.pes_cw._from_internal([1.854, 3.631, 0.0])
+        x = np.column_stack((x0, x1, x2, x3))
+        self.pes_cw._calculate_adiabatic_all(x, None)
+        energy_ref = np.array([[0.012107, -0.00139, 0.004114, 0.012107]])
+        gradient_ref = np.zeros((1, 9, 4))
+        np.testing.assert_allclose(self.pes_cw._adiabatic_energy, energy_ref, atol=1e-5)
+        np.testing.assert_allclose(self.pes_cw._adiabatic_gradient, gradient_ref, atol=1e-4)
+        
+    def test_minimize(self):
+        # BKMP2
+        fun_ref = -0.17449577
+        x_ref = np.array([40.0, 0.0, 0.0, 0.70073594, 0.0, 0.0, -0.70073594, 0.0, 0.0])
+        fun, x = self.pes_bkmp2.minimize_geom(self.pes_bkmp2._from_internal([2.0, 40.0, 0.0]))
+        self.assertAlmostEqual(fun_ref, fun)
+        np.testing.assert_allclose(x, x_ref)
+
+        # LWAL
+        fun_ref = 0.0
+        x_ref = np.array([100.0, 0.0, 0.0, -0.700584, 0.0, 0.0, 0.700584, 0.0, 0.0])
+        fun, x = self.pes_lwal.minimize_geom(self.pes_lwal._from_internal([1.4, 100.0, 0.0]))
+        np.testing.assert_allclose(fun_ref, fun, atol=1e-5)
+        np.testing.assert_allclose(x, x_ref, atol=1e-5)
+
+        # CW
+        # H2
+        fun_ref = - 0.00139 # lowered by SO
+        x_ref = np.array([40.0, 0.0, 0.0, -0.700279, 0.0, 0.0, 0.700279, 0.0, 0.0])
+        fun, x = self.pes_cw.minimize_geom(self.pes_cw._from_internal([1.4, 40.0, 0.0]))
+        np.testing.assert_allclose(fun_ref, fun, atol=1e-6)
+        np.testing.assert_allclose(x, x_ref, atol=1e-3)
+
+        # HCl
+        fun_ref = 0.004114
+        x_ref = np.array([62.41003, 0.0, 0.0, -60.0, 0.0, 0.0, 60.0, 0.0, 0.0])
+        fun, x = self.pes_cw.minimize_geom(self.pes_cw._from_internal([120.0, 60.0+2.410, 0.0]))
+        np.testing.assert_allclose(fun_ref, fun, atol=1e-6)
+        np.testing.assert_allclose(x, x_ref, atol=1e-3)
+        
+    def test_get_Hessian(self):
+        # BKMP2
+        freq_ref = np.zeros(9)
+        freq_ref[8] = 4403
+        r = self.pes_bkmp2._from_internal([1.4014718, 80.0, 0.0])
+        hessian = self.pes_bkmp2.get_Hessian(r)
+        freq = nm.get_normal_modes(hessian, [units.atom_mass('H')]*9)[0]*units.nm_to_cm
+        np.testing.assert_allclose(freq, freq_ref, atol=4.0)
+
+        freq_ref = np.zeros(9)
+        freq_ref[0] = -1510
+        freq_ref[6] = 907
+        freq_ref[7] = 907
+        freq_ref[8] = 2055
+        r = self.pes_bkmp2._from_internal([1.757, 2.6355, 0.0])
+        hessian = self.pes_bkmp2.get_Hessian(r)
+        freq = nm.get_normal_modes(hessian, [units.atom_mass('H')]*9)[0]*units.nm_to_cm
+        np.testing.assert_allclose(freq, freq_ref, atol=4.0)
+
+        # LWAL 
+        freq_ref = np.zeros(9)
+        freq_ref[8] = 4406
+        r = self.pes_lwal._from_internal([1.401168, 300.0, 0.0])
+        hessian = self.pes_lwal.get_Hessian(r)
+        freq = nm.get_normal_modes(hessian, [units.atom_mass('F')]*3 + [units.atom_mass('H')]*6)[0]*units.nm_to_cm
+        np.testing.assert_allclose(freq, freq_ref, atol=5.0)
+
+        # TODO: Same as from instanton code - but different from paper; why?
+        freq_ref = np.zeros(9)
+        freq_ref[0] = -715.8
+        freq_ref[7] = 326.4
+        freq_ref[8] = 3859.1
+        R = np.sqrt( (1.457/2.0)**2 + 2.932**2 - 1.457*2.932*np.cos(np.pi*113.0/180.0))
+        gamma = np.arccos((2.932**2 - R**2 - (1.457/2.0)**2) / (-R * 1.457))
+        r = self.pes_lwal._from_internal([1.457, R, gamma])
+        hessian = self.pes_lwal.get_Hessian(r)
+        freq = nm.get_normal_modes(hessian, [units.atom_mass('F')]*3 + [units.atom_mass('H')]*6)[0]*units.nm_to_cm
+        np.testing.assert_allclose(freq[[0, 7, 8]], freq_ref[[0, 7, 8]], atol=5.0)
+
+        # CW
+        # HCl
+        freq_ref = np.zeros(9)
+        freq_ref[8] = 2989
+        r = self.pes_cw._from_internal([120.0, 60.0+2.41003, 0.0])
+        hessian = self.pes_cw.get_Hessian(r)
+        freq = nm.get_normal_modes(hessian, [units.atom_mass('Cl')]*3 + [units.atom_mass('H')]*6)[0]*units.nm_to_cm
+        np.testing.assert_allclose(freq, freq_ref, atol=10.0)
+        
+        freq_ref = np.zeros(9)
+        freq_ref[8] = 4403
+        r = self.pes_cw._from_internal([1.4005706, 21.0, 0.0])
+        hessian = self.pes_cw.get_Hessian(r)
+        freq = nm.get_normal_modes(hessian, [units.atom_mass('Cl')]*3 + [units.atom_mass('H')]*6)[0]*units.nm_to_cm
+        np.testing.assert_allclose(freq, freq_ref, atol=10.0)
+
+        freq_ref = np.zeros(9)
+        freq_ref[0] = -1296
+        freq_ref[6] = 540
+        freq_ref[7] = 540
+        freq_ref[8] = 1360
+        r = self.pes_cw._from_internal([1.854, 3.631, 0.0])
+        hessian = self.pes_cw.get_Hessian(r)
+        freq = nm.get_normal_modes(hessian, [units.atom_mass('Cl')]*3 + [units.atom_mass('H')]*6)[0]*units.nm_to_cm
+        np.testing.assert_allclose(freq, freq_ref, atol=10.0)
+        
+    def test_from_internal(self):
+        # colinear
+        internal = np.array([2.0, 4.0, 0.0])
+        cartesian_ref = np.array([4.0, 0.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0, 0.0])
+        cartesian = self.pes_bkmp2._from_internal(internal)
+        np.testing.assert_allclose(cartesian, cartesian_ref)
+        np.testing.assert_allclose(internal, self.pes_bkmp2._from_cartesian_to_internal(cartesian))
+
+        # perpendicular
+        internal = np.array([2.0, 4.0, np.pi/2.0])
+        cartesian_ref = np.array([ 0.0, 4.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0, 0.0])
+        cartesian = self.pes_bkmp2._from_internal(internal)
+        np.testing.assert_allclose(cartesian, cartesian_ref, atol=1e-10)
+        np.testing.assert_allclose(internal, self.pes_bkmp2._from_cartesian_to_internal(cartesian))
+
+        # 45 degrees off
+        internal = np.array([2.0, 4.0, np.pi/4.0])
+        cartesian_ref = np.array([4.0/np.sqrt(2.0), 4.0/np.sqrt(2.0), 0.0, -1.0, 0.0, 0.0, 1.0, 0.0, 0.0])
+        cartesian = self.pes_bkmp2._from_internal(internal)
+        np.testing.assert_allclose(cartesian, cartesian_ref)
+        np.testing.assert_allclose(internal, self.pes_bkmp2._from_cartesian_to_internal(cartesian))
+
+        # -45 degrees off
+        internal = np.array([2.0, 4.0, 2.0*np.pi-np.pi/4.0])
+        cartesian_ref = np.array([4.0/np.sqrt(2.0), -4.0/np.sqrt(2.0), 0.0, -1.0, 0.0, 0.0, 1.0, 0.0, 0.0])
+        cartesian = self.pes_bkmp2._from_internal(internal)
+        np.testing.assert_allclose(cartesian, cartesian_ref)
+        np.testing.assert_allclose(internal, self.pes_bkmp2._from_cartesian_to_internal(cartesian))
+
+    def test_from_cartesian_to_internal(self):
+        # colinear
+        cartesian = np.array([3.8, 0.0, 0.0, -1.2, 0.0, 0.0, 1.2, 0.0, 0.0])
+        internal_ref = np.array([2.4, 3.8, 0.0])
+        internal = self.pes_bkmp2._from_cartesian_to_internal(cartesian)
+        np.testing.assert_allclose(internal, internal_ref)
+        np.testing.assert_allclose(cartesian, self.pes_bkmp2._from_internal(internal))
+
+        # perpendicular
+        cartesian = np.array([4.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0])
+        internal_ref = np.array([2.0, 4.0, np.pi/2.0])
+        internal = self.pes_bkmp2._from_cartesian_to_internal(cartesian)
+        np.testing.assert_allclose(internal, internal_ref)
+
+        # 'random' in space, 1st H along axis
+        cartesian = np.array([-1.25 , -3.25, -1.18198052, 1.0, -1.0, 2.0, 0.5, -1.5, 2.0-1.0/np.sqrt(2.0)])
+        internal_ref = np.array([1.0, 4.0, 2.0*np.pi])
+        internal = self.pes_bkmp2._from_cartesian_to_internal(cartesian)
+        np.testing.assert_allclose(internal, internal_ref)
+
+        # -45 degrees off
+        cartesian = np.array([4.0/np.sqrt(2.0), -4.0/np.sqrt(2.0), 0.0, -1.0, 0.0, 0.0, 1.0, 0.0, 0.0])
+        internal_ref = np.array([2.0, 4.0, 2.0*np.pi-np.pi/4.0])
+        internal = self.pes_bkmp2._from_cartesian_to_internal(cartesian)
+        np.testing.assert_allclose(internal, internal_ref)
+        np.testing.assert_allclose(cartesian, self.pes_bkmp2._from_internal(internal))
+
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(TriatomicTest)
+    unittest.TextTestRunner().run(suite)

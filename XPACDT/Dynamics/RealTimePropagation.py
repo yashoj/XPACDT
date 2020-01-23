a0229635c03fd1f9b2694630ced15818369c7a97
Ralph Welsch <ralph.welsch@cfel.de>
2020-01-23 09:37:46 +0100

    Fix for XPACDT-112. Now explicitly testing (and creating) folder in real time propagation.

--- XPACDT/Dynamics/RealTimePropagation.py
+++ XPACDT/Dynamics/RealTimePropagation.py
@@ -83,8 +83,15 @@ def propagate(system, input_parameters):
 
     # set up pickle file
     name_folder = sys_parameters.get('folder')
+    if not os.path.isdir(name_folder):
+        try:
+            os.mkdir(name_folder)
+        except OSError:
+            sys.stderr.write("Creation of trajectory folder failed!")
+            raise
     name_file = sys_parameters.get('picklefile', 'pickle.dat')
     path_file = os.path.join(name_folder, name_file)
+    
 
     while(system.nuclei.time < time_end):
         system.step(timestep_output, True)

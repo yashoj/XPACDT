6b982a9b97a931c57dca4847e69d61c07e5251f9
Ralph Welsch <ralph.welsch@cfel.de>
2020-02-04 10:09:13 +0100

    Updated full input.

--- XPACDT/Interfaces/MorseDiabatic.py
+++ XPACDT/Interfaces/MorseDiabatic.py
@@ -56,7 +56,7 @@ class MorseDiabatic(itemplate.PotentialInterface):
     ----------------
     model_type : {'model_1', 'model_2', 'model_3'}
         String denoting model number to be used.
-    n_states : int or string of int
+    n_states : string of int
         Number of morse diabatic states (possible: 2, 3).
     """
 
@@ -65,7 +65,7 @@ class MorseDiabatic(itemplate.PotentialInterface):
         pes_parameters = parameters.get("MorseDiabatic")
 
         try:
-            n_states = int(pes_parameters.get('n_states', 3))
+            n_states = int(pes_parameters.get('n_states'))
         except ValueError as e:
             raise type(e)(str(e) + "\nXPACDT: Parameter 'n_states' for morse "
                                    "diabatic not convertable to int. "
